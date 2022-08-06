4b6fb50a2c7f3a69db8bd75bd8c4c80b104a5d01
maiico <michael.nguyen134@gmail.com>
2022-08-06 12:19:57 -0400

    added hero to homescreen

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,13 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, createRef} from 'react';
 import './Home.css';
 import Projects from './components/projects/Projects';
 import Team from './components/team/Team'
 import config from '../../Config.json';
 import axios from 'axios';
+import FirstHeroImg from '../images/hero-img-1.png';
+import SecondHeroImg from '../images/hero-img-2.png';
 
 function Home() {
 
     const [solPriceUsd, setSolPriceUsd] = useState(0);
+    const scrollDiv = createRef();
 
     useEffect(() => {
 
@@ -28,12 +31,21 @@ function Home() {
         <div className='home'>
             <div className="home-hero">
                 <div className="home-content-container">
-                    <h1 className="home-heading">We like JPEGS.</h1>
-                    <h2 className="home-subheading">We are a collective of Analysts, Developers, Holders and Degenerates providing analytics and insights into NFT projects on Solana.</h2>
+                    <div className="home-txt-container">
+                        <h1 className="home-heading">We like JPEGS.</h1>
+                        <h2 className="home-subheading">Get quick analytics and insights on top-rated NFT projects on Solana.</h2>
+                        <h3 className="home-subheading-2">Fast-track DYOR by discovering all you need to know about an NFT project. All in one place.</h3>
+                        <button className="discover-button"           
+                            onClick={() => {scrollDiv.current.scrollIntoView({ behavior: 'smooth' })}}>Discover Top-Rated NFTs</button>
+                    </div>
+                    <div className="hero-img-container">
+                        <img src={FirstHeroImg} className="hero-img-1" alt="charts" />
+                        <img src={SecondHeroImg} className="hero-img-2" alt="charts" />
+                    </div>
                 </div>
             </div>
-            <div className="discover-nfts-container">
-                <h1 className="home-subheading-2">Discover Top-Rated NFTs</h1>
+            <div className="discover-nfts-container" ref={scrollDiv}>
+                <h1 className="home-subheading-gradient">Discover Top-Rated NFTs</h1>
                 <Projects solPriceUsd={solPriceUsd}/>
             </div>
             <div className="team">
